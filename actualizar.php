63391c982c4e66107d5f6799cc53ffc2509657ee
Danx398 <danidannniel398@gmail.com>
2021-12-13 08:32:16 -0600

    CRUD

---
+++ actualizar.php
@@ -0,0 +1,43 @@
+
+<?php 
+  require_once "clases/crud.php";
+  $crud = new Crud();
+  $id = $_POST['_idActualizar'];
+  $datos = $crud -> obtenerDocumento($id);
+  // echo $id;
+  $idMongo = new MongoDB\BSON\ObjectId($datos -> _id);
+?>
+
+<?php include "header.php" ?>
+
+<div class="jumbotron jumbotron-fluid">
+  <div class="container bg-light">
+    <h1 class="display-4 text-center"><i class="fas fa-edit"></i> Actualizar un registro </h1>
+    <hr>
+    <p class="lead">
+        <form action="procesos/actualizar.php" method="POST">
+            <input type="text" hidden name="idActualizar" value="<?php echo $idMongo; ?>" >
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" class="form-control" required value="<?php echo $datos -> nombre; ?>">
+            <label for="paterno">Apellido Paterno</label>
+            <input type="text" name="Apaterno" class="form-control" required value="<?php echo $datos -> paterno; ?>">
+            <label for="materno">Apellido materno</label>
+            <input type="text" name="Amaterno" class="form-control" required value="<?php echo $datos -> materno; ?>">
+            <label for="fecha_nacimiento">Fecha de nacimiento</label>
+            <input type="date" name="fechaNac" class="form-control" required value="<?php echo $datos -> fecha_nacimiento; ?>">
+            <br>
+                <button class="btn btn-warning btn-block"><i class="fas fa-edit"></i> Actualizar </button>
+            </form>
+            <!-- <button class="btn btn-outline-warning btn-block"><i class="fas fa-edit"></i> Actualizar </button> -->
+            <br>
+            <a href="index.php" class="btn btn-dark btn-block"> 
+            <i class="fas fa-backward"></i>
+              Regresar
+            </a>
+            <br>
+        </form>
+    </p>
+  </div>
+</div>
+
+<?php include "scripts.php" ?>
---
+++ eliminar.php
@@ -0,0 +1,64 @@
+
+<?php 
+
+    require_once "clases/crud.php";
+
+    // llamare la clase Crud del documento crud para acceder a las variables de lo datos
+    $obj = new Crud();
+    // arreglo de los datos con la variable global POST
+    $id = $_POST['_id'];
+
+    $datos = $obj -> obtenerDocumento($id);
+    // echo ($id);
+    $idMongo = new MongoDB\BSON\ObjectId($datos -> _id);
+    // echo $datos -> nombre;
+
+?>
+
+<?php require_once "header.php" ?>
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container bg-light">
+        <h1 class="display-4"><i class="fas fa-trash-alt"></i> Eliminar</h1>
+        <hr>
+        <p class="lead">
+            <div class="alert alert-danger" role="alert">
+                <h4 class="alert-heading">Advertencia!</h4>
+                <p>Estas seguro de elimiar este registro ¿?</p>
+                <hr>
+                <div class="table-responsive">
+                    <table class="table table-hover table-sm table-bordered">
+                        <thead>
+                            <th>Nombre</th>
+                            <th>Apellido Materno</th>
+                            <th>Apellido Paterno</th>
+                            <th>Fecha de nacimiento</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?php echo $datos -> nombre; ?></td>
+                                <td><?php echo $datos -> paterno; ?></td>
+                                <td><?php echo $datos -> materno; ?></td>
+                                <td><?php echo $datos -> fecha_nacimiento; ?></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <p class="mb-0 mt-4">
+                    <form action="procesos/eliminar.php" method="POST">
+                        <input type="text" hidden name="idEliminar" value="<?php echo $idMongo; ?>" >
+                        <button class="btn btn-danger btn-block"><i class="fas fa-trash-alt"></i> Eliminar</button>
+                    </form>
+                    <br>
+                    <a href="index.php" class="btn btn-dark btn-block">
+                    <i class="fas fa-backward"></i>
+                        Regresar
+                    </a>
+                </p>
+            </div>
+        </p>
+        <br>
+    </div>
+</div>
+
+<?php require_once "scripts.php" ?>
